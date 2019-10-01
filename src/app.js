c8d09860d81062baf31a30baa80a4c62ce1547f3
Cam Prier <CSPrier@gmail.com>
2019-10-01 15:27:14 -0400

    ES6 node express server commit. Complete with scripts for different environments

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index';
+
+// Instance
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Static
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.use('/', indexRouter);
+
+
+
+export default app;
