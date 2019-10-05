54dbfdc4222c27f77238ee6c1c4231a1a3f59ffa
Cam Prier <CSPrier@gmail.com>
2019-10-05 18:13:06 -0400

    Added cors dotenv passport jwt and error handling

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+// ===============================================================================================
+// Require dotenv
+require('dotenv').config();
+
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
@@ -7,6 +12,20 @@ import indexRouter from './routes/index';
 // Instance
 const app = express();
 
+// ===============================================================================================
+// CORS
+app.use(
+  cors({ origin: CLIENT_ORIGIN })
+);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-Access-Token, XKey, Authorization');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.options('*', cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +33,22 @@ app.use(cookieParser());
 
 // Static
 app.use(express.static(path.join(__dirname, '../public')));
+
+// ERROR HANDLING
+app.use((req, res, next) => {
+  const err = new Error('Not Founds, something broke!');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(`ERROR: ${err}`);
+  res.status(err.status);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : err
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
