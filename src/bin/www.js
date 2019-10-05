3dfa1d7fbcdeaf091fca7eae6e5692cd7520ce6f
Cam Prier <CSPrier@gmail.com>
2019-10-05 18:32:26 -0400

    Created server.utils.js and exported an object with the functions needed in www.js

--- src/bin/www.js
+++ src/bin/www.js
@@ -4,12 +4,13 @@
 
 // Module dependencies.
 import app from '../app';
+import { serverUtils } from '../lib/server.utils';
 import debugLib from 'debug';
 import http from 'http';
 const debug = debugLib('cspChat:server');
 
 // Get port from environment and store in Express.
-const port = normalizePort(process.env.PORT || '8080');
+const port = serverUtils.normalizePort(process.env.PORT || '8080');
 app.set('port', port);
 
 // Create HTTP server.
@@ -19,8 +20,8 @@ const server = http.createServer(app).listen(port, () => {
 const io = require('socket.io').listen(server);
 
 // Listen on provided port, on all network interfaces.
-server.on('error', onError);
-server.on('listening', onListening);
+server.on('error', (err) => serverUtils.onError(err));
+server.on('listening', serverUtils.onListening());
 
 /** SOCKETS */
 // io listen on the connection event for incoming sockets
@@ -28,63 +29,13 @@ io.on('connection', (socket) => {
   // connect
   console.log('a user connected');
   // disconnect
-  socket.on('disconnect', function(){
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
   });
 });
-
-// Normalize a port into a number, string, or false.
-function normalizePort(val) {
-  let port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-  return false;
-}
-
-// Event listener for HTTP server "error" event.
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  let bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-// Event listener for HTTP server "listening" event.
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
