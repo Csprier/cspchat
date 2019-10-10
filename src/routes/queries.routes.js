468f9e6b36d3078ad2aa516aaf623a5160633233
Cam Prier <CSPrier@gmail.com>
2019-10-10 16:51:49 -0400

    Added POST query

--- src/routes/queries.routes.js
+++ src/routes/queries.routes.js
@@ -2,6 +2,7 @@
 import express from 'express';
 const router = express.Router();
 
+// GET
 const getUsers = (req, res) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (err, result) => {
     if (err) {
@@ -11,6 +12,7 @@ const getUsers = (req, res) => {
   });
 };
 
+// GET/:id
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
   pool.query('SELECT * FROM users WHERE id = $1', [id], (err, result) => {
@@ -19,6 +21,26 @@ const getUserById = (req, res) => {
     }
     res.status(200).json(result.rows);
   });
+};
+
+// POST
+const createUser = (req, res) => {
+  const { username, email } = req.body;
+
+  pool.query(`
+      INSERT INTO users 
+      (username, email), 
+      VALUES
+      $1, $2
+    `, 
+    [ username, email ],
+    (err, result) => {
+      if (err) {
+        throw new Error(`There was an error creating a new user: ${err}`);
+      }
+      res.status(200).json(result.rows);
+    }
+  );
 };
 
 export default router;
